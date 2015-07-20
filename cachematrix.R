37ee6209166878e1b196e541b3df26d794ec4748
Jenny Chu <jchu678@gmail.com>
2015-07-19 21:33:40 -0700

    completed R compramming assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of the R functions below is to cache potentially time-consuming computations, such as mean, inverse of a matrix, etc.
+## It can be achieve by taking advantage of the scoping rules of the R language to preserve state inside of an R object.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) i <<- solve
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        mat.data <- x$get()
+        i <- solve(mat.data, ...)
+        x$setinv(i)
+        i
 }
+
